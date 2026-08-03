1f79dce227f93289d9ca23f7495662279c8d4dff
Salsa Paringga Pangestu N <128108996+SalsaParingga@users.noreply.github.com>
2026-08-03 16:33:16 +0700

    Update udjaya.py

--- udjaya.py
+++ udjaya.py
@@ -1020,7 +1020,6 @@ if (
                 # Cek apakah semua selesai
                 #==========================
                 time.sleep(1)
-                st.write(df_monitor.dtypes)
                 st.write(df_monitor)
                 data_monitor = ws.get(
                     "A:K",
